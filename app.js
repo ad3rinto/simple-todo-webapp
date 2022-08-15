2de3c93303a9ff7ac59a88d280d62be1c264a3e9
ad3rinto <aad3rinto@gmail.com>
2022-08-15 12:20:42 +0100

    adding ejs components

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { RSA_NO_PADDING } = require("constants");
 const PORT = 3000;
 
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.send("Working")
+    var today = new Date();
+    var day = "";
+
+    if(today.getDay() === 6 || today.getDay() === 0){
+        day = "Weekend";
+        res.render("list", {kindOfDay: day})
+
+    } else {
+        day = "Weekday";
+        res.render("list", {kindOfDay: day})
+    }
 })
 
 app.listen(PORT, function(){
     console.log(`Server started on port ${PORT}`)
 })
-
