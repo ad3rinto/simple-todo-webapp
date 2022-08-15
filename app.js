03240a8014b4129a10fbce538ca86dba3098b153
ad3rinto <aad3rinto@gmail.com>
2022-08-15 15:33:35 +0100

    more updates

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     var today = new Date();
-    var day = "";
+    var dayOfTheWeek = today.toLocaleString(
+        'default', {weekday: 'long'}
+    );
+    console.log(dayOfTheWeek)
 
-    if(today.getDay() === 6 || today.getDay() === 0){
-        day = "Weekend";
-        res.render("list", {kindOfDay: day})
-
-    } else {
-        day = "Weekday";
-        res.render("list", {kindOfDay: day})
-    }
-})
+    // if(today.getDay() === 6 || today.getDay() === 0){
+    //     dayOfTheWeek = "Weekend";
+    // } else {
+    //     dayOfTheWeek = "Weekday";        
+    // }
+    res.render("list", {kindOfDay: dayOfTheWeek});
+});
 
 app.listen(PORT, function(){
     console.log(`Server started on port ${PORT}`)
