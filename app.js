404d8d7d00d19d29a62b5427a26dd1b797d0802d
ad3rinto <aad3rinto@gmail.com>
2022-08-18 08:29:49 +0100

    added list & form elements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { RSA_NO_PADDING } = require("constants");
+const { name } = require("ejs");
 const PORT = 3000;
 
 
@@ -13,11 +14,20 @@ app.get("/", function(req, res){
     var dayOfTheWeek = today.toLocaleString(
         'default', {weekday: 'long'}
     );
-    console.log(dayOfTheWeek)
 
-    
     res.render("list", {kindOfDay: dayOfTheWeek});
 });
+
+
+app.post("/", function(req, res){
+    var todoItem = req.body.nextItem;
+    console.log(todoItem);
+
+    res.redirect("/")
+})
+
+
+
 
 app.listen(PORT, function(){
     console.log(`Server started on port ${PORT}`)
