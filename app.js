70f99ad72fcf8af37a6b6accda14ef4ad965088e
ad3rinto <aad3rinto@gmail.com>
2022-08-18 14:18:23 +0100

    implemnted loops for li items

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { RSA_NO_PADDING } = require("constants");
+const { RSA_NO_PADDING, SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require("constants");
 const { name } = require("ejs");
 const PORT = 3000;
-
+var items = ["Buy Food","Cook Food","Eat Food"];
 
 const app = express();
 app.set("view engine", "ejs");
@@ -15,13 +15,14 @@ app.get("/", function(req, res){
         'default', {weekday: 'long'}
     );
 
-    res.render("list", {kindOfDay: dayOfTheWeek});
+    res.render("list", {kindOfDay: dayOfTheWeek,todoItems : items});
 });
 
 
 app.post("/", function(req, res){
     var todoItem = req.body.nextItem;
-    console.log(todoItem);
+    items.push(todoItem);
+    console.log(items)
 
     res.redirect("/")
 })
